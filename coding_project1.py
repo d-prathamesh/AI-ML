a6c04c78634a939c68fcc1870e909c07f651cead
Prathamesh Deokar <69900104+d-prathamesh@users.noreply.github.com>
2023-11-13 17:57:11 +0530

    Create coding_project1.py

---
+++ coding_project1.py
@@ -0,0 +1,10 @@
+# This is one line comment.
+"""
+This
+is 
+Multiline
+Comment
+"""
+print("Hello world")
+name = "Prathamesh" #this is variable syntax for python
+print(name) # this is printing variable
