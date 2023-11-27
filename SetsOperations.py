bc39db00db94fa0a6497a21187597414374a0f2f
Prathamesh Deokar <69900104+d-prathamesh@users.noreply.github.com>
2023-11-27 17:51:42 +0530

    Create SetsOperations.py

---
+++ SetsOperations.py
@@ -0,0 +1,12 @@
+E = {0,2,4,6,8}
+N = {1,3,2,4,5}
+
+union_of_en = E.union(N)
+intersection_of_en = E.intersection(N)
+Difference_of_en = E.difference(N)
+Symmetric_Difference_of_en = E.symmetric_difference(N)
+
+print(union_of_en)
+print(intersection_of_en)
+print(Difference_of_en)
+print(Symmetric_Difference_of_en)
